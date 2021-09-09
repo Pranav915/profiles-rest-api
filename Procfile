e363287d44992095c27a13bdb104eca69399bdbb
Pranav <patilpranav915@gmail.com>
2021-09-09 11:56:14 +0530

    Added requirements.txt & Procfile

--- Procfile
+++ Procfile
@@ -1,3 +1,1 @@
-web: gunicorn profiles_project.wsgi:application --log-file - --log-level debug
-python manage.py collectstatic --noinput
-manage.py migrate
+web: gunicorn profiles_project.wsgi --log-file - 
--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d9he1ozz#))u-ug0^z0o2)0fu%n)rby)f%x0)t4nqh#3_$g!fv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
-    'evening-earth-24227.herokuapp.com',
+    'pranav915-profile-rest-api.herokuapp.com',
     'localhost'
 ]
 
